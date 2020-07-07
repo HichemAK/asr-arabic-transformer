1fd6249d8f72ab49b316f2a37a7028977587b2bb
AMMAR KHODJA Hichem <hichem5696@gmail.com>
2020-07-07 17:36:42 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="shade22413",
+    name="asr-transformer-transformer",
     version="0.0.1",
     author="AMMAR KHODJA Hichem | BOUDJENIBA Oussama",
     author_email="hichem5696@gmail.com",
