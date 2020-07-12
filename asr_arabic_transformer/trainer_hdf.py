ce4297e64c13883b0bdf7949c4355a39225f9cc0
AMMAR KHODJA Hichem <hichem5696@gmail.com>
2020-07-12 19:47:03 +0200

    accuracy implementation

--- asr_arabic_transformer/trainer_hdf.py
+++ asr_arabic_transformer/trainer_hdf.py
@@ -1,7 +1,7 @@
 import copy
-import torch
 from os import path
-from asr_arabic_transformer.utils import shuffle_jointly
+
+import torch
 from pandas import HDFStore
 
 
@@ -48,16 +48,21 @@ class TrainerHDF:
                     print("Iteration :", len(train_loss_history), "Loss :", loss / count)
 
             valid_loss = 0
+            accuracy = 0
             loss /= count
             count = 0
             self.model.eval()
             for x, target in valid_gen:
-                valid_loss += self.eval_batch(x, target)
+                vl, acc = self.eval_batch(x, target)
+                valid_loss += vl
+                accuracy += acc
                 valid_loss_history += [valid_loss] * (len(valid_loss_history) - len(train_loss_history))
                 count += 1
             valid_loss /= count
+            accuracy /= count
             print("Training Loss   :", loss)
             print("Validation Loss :", valid_loss)
+            print("Validation accuracy :", accuracy)
 
             if valid_loss < best_loss:
                 print("New best reached!   Saving Model...")
@@ -83,8 +88,8 @@ class TrainerHDF:
         if self.device == 'cuda':
             x, target = x.cuda(), target.cuda()
         self.optimizer.zero_grad()
-        out = self.model(x, target[:,:-1])
-        loss = self.loss_function(out, target[:,1:])
+        out = self.model(x, target[:, :-1])
+        loss = self.loss_function(out, target[:, 1:])
         loss.backward()
         self.optimizer.step()
         return loss.item()
@@ -92,6 +97,7 @@ class TrainerHDF:
     def eval_batch(self, x, target):
         if self.device == 'cuda':
             x, target = x.cuda(), target.cuda()
-        out = self.model(x, target[:,:-1])
-        loss = self.loss_function(out, target[:,1:])
-        return loss.item()
+        out = self.model(x, target[:, :-1])
+        loss = self.loss_function(out, target[:, 1:])
+        accuracy = int(torch.all(out.argmax(dim=-1) == target[:, 1:], dim=-1).to(torch.int).sum()) / out.shape[0]
+        return loss.item(), accuracy
