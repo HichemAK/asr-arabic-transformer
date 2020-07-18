75242c9eb9a15304a4c70dcc71246822f0aa768d
AMMAR KHODJA Hichem <hichem5696@gmail.com>
2020-07-18 19:49:18 +0200

    RNN Attention

--- asr_arabic_transformer/attention/speech_rnn.py
+++ asr_arabic_transformer/attention/speech_rnn.py
@@ -1,8 +1,11 @@
+from torch import nn
+
 from asr_arabic_transformer.attention.attention_rnn import Attention_RNN
 
 
-class SpeechRNN():
+class SpeechRNN(nn.Module):
     def __init__(self, input_size, n_classes, Ty):
+        super().__init__()
         self.input_size = input_size
         self.n_classes = n_classes
         self.Ty = Ty
