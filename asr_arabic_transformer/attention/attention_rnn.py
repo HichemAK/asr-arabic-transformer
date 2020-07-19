8f2c31866098bf8eff4d44096c2f3bd9eb598cfe
AMMAR KHODJA Hichem <hichem5696@gmail.com>
2020-07-19 12:37:09 +0200

    SpeechRNN config

--- asr_arabic_transformer/attention/attention_rnn.py
+++ asr_arabic_transformer/attention/attention_rnn.py
@@ -6,15 +6,16 @@ import torch.nn.functional as F
 class Attention_RNN(nn.Module):
     """Class containing the architecture of the model and the corresponding weights
     Contains a classical implementation of attention model"""
-    def __init__(self, num_alphabet, Ty, project_size=25, encoder_hidden_size=128,
+    def __init__(self, input_size, num_alphabet, Ty, project_size=25, encoder_hidden_size=128,
                  encoder_num_layers=2, decoder_hidden_size=128, decoder_num_layers=1, save_attention=False):
         super().__init__()
         self.num_alphabet = num_alphabet
+        self.input_size = input_size
         self.Ty = Ty
         self.Tx = None
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
 
-        self.fc1 = nn.Linear(self.num_alphabet, project_size)
+        self.fc1 = nn.Linear(input_size, project_size)
         self.encoder = nn.LSTM(project_size, hidden_size=encoder_hidden_size, num_layers=encoder_num_layers,
                                bidirectional=True, batch_first=True)
         self.post_attention_lstm = nn.LSTM(self.encoder.hidden_size * 2, hidden_size=decoder_hidden_size,
--- asr_arabic_transformer/attention/speech_rnn.py
+++ asr_arabic_transformer/attention/speech_rnn.py
@@ -10,11 +10,20 @@ class SpeechRNN(nn.Module):
         self.input_size = input_size
         self.n_classes = n_classes
         self.Ty = Ty
-        self.attention_rnn = Attention_RNN(input_size, n_classes, Ty, project_size=25, encoder_hidden_size=128,
-                                           encoder_num_layers=2, decoder_hidden_size=128, decoder_num_layers=1)
+        self.attention_rnn = Attention_RNN(input_size, n_classes, Ty, project_size=project_size,
+                                           encoder_hidden_size=encoder_hidden_size,
+                                           encoder_num_layers=encoder_num_layers,
+                                           decoder_hidden_size=decoder_hidden_size,
+                                           decoder_num_layers=decoder_num_layers)
 
     def forward(self, x):
         return self.attention_rnn(x)
 
     def predict(self, x, eos, max_iter=100):
         return self.attention_rnn.predict(x, eos, max_iter)
+
+
+if __name__ == '__main__':
+    model = SpeechRNN(10, 10, 20,
+                      project_size=64, encoder_hidden_size=256,
+                      encoder_num_layers=2, decoder_hidden_size=256, decoder_num_layers=1)
